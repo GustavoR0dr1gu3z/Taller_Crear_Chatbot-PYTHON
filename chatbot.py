0e3f568fd62d5a673f97cfab0c1771aec5a9f2bb
Gustavo Rodríguez Calzada <gustavo.soader.cx@gmail.com>
2021-07-02 20:49:45 -0500

    Arreglando errores

--- chatbot.py
+++ chatbot.py
@@ -370,7 +370,7 @@ def Raiz(inp):
 def chat():
     print("\nChatBot: Hola soy un chatbot, comienza a hablar conmigo\n")
     while True:
-        inp = str(input("    Tú: "))
+        inp = input("   Tú: ")
 
         # Instrucción de fin de conversación (Cerrar el proceso)
         if inp.lower() == "Salir":
